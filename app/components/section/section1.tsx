a33e02b1e2255b262dc231bb5ede66e6188acf96
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-03-05 20:57:20 -0300

    mudanças

--- app/components/section/section1.tsx
+++ app/components/section/section1.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
+import Sectionimg from '../../assets/fundo.jpg';
 
+const sectionStyle: React.CSSProperties = {
+  width: '100%',
+  height: '704px',
+  backgroundImage: `url(${Sectionimg})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  border: '1px solid #000',
+};
 
 function Section1() {
-  const sectionStyle = {
-    width: '100%',
-    height: '704px',
-    backgroundImage: "url('../../assets/section1.jpg')",
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    zIndex: 100
-  };
-
   return (
     <section style={sectionStyle}>
       
