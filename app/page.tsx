b96a1e29d8b4143801b1c92e610359c39eb79c16
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-03-05 21:57:35 -0300

    ajustes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import LandingPage from "@/app/pages/ladingpage/landingpage";
+import LandingPage from "@/app/pages/ladingpage/page";
+
 
 export default function Home() {
   return (
     <>
-      <LandingPage></LandingPage>
+      
     </>
   );
 }
--- app/pages/ladingpage/page.tsx
+++ app/pages/ladingpage/page.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import fundo from "../../assets/fundo.jpg";
+import NavBar from "../../components/navbar/Navbar"
 
 const Landing = () => {
   return (
     <>
+    <NavBar />
       <Image className="w-full z-0" src={fundo} alt="fundo"></Image>
     </>
   );
