018fdd3c9c0c7a0f5eee03b9b4b1a9c9601d92ea
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-03-05 22:13:03 -0300

    consertando a landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
-import LandingPage from "@/app/pages/ladingpage/page";
+import NavBar from "./components/navbar/Navbar";
+import fundo from "./assets/fundo.jpg";
+
 
 
 export default function Home() {
   return (
     <>
-      
+    <NavBar></NavBar>
+        <Image className="w-full z-0" src={fundo} alt="fundo"></Image>
+
     </>
   );
-}
+} 
